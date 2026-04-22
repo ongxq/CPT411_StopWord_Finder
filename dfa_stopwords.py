8bb6bc46bc70bd1d1398b669cbdff628c686f584
ONG XIU QIN <xqwow1024@gmail.com>
2026-04-22 20:44:08 +0800

    dfa

--- dfa_stopwords.py
+++ dfa_stopwords.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import re
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -236,7 +237,8 @@ def process():
 # RUN
 # =========================
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
